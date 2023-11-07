1ce14054f472fd76cab3a80d4485369421c83de3
endeavorXx <endeavorxx@Ubuntu1804.myguest.virtualbox.org>
2023-11-07 19:30:05 +0530

    new commit

--- mems2.c
+++ mems2.c
@@ -84,13 +84,18 @@ void mems_finish() {
         }else{
             temp = NULL;
         }
-        munmap(node,node->pages*PAGE_SIZE);
+        int t = munmap(node,node->pages*PAGE_SIZE);
+        // if (t == -1){
+        //     perror("munmap");
+        //     // exit(EXIT_FAILURE);
+        // }
         node = temp;
-        printf("Exiting out\n");
+        // printf("Exiting out\n");
         count--;
     }
     head->next = NULL;
     head = NULL;
+    count = 0;
 }
 
 /*
@@ -426,6 +431,7 @@ void mems_free(void* v_ptr) {
 
 
 int main() {
+    // printf("%d\n",(int*) mems_get((int*)0));
     mems_init();
     int* ptr[10];
 
@@ -437,26 +443,40 @@ int main() {
         ptr[i] = (int*)mems_malloc(sizeof(int)*250);
         printf("Virtual address: %lu\n", (unsigned long)ptr[i]);
     }
-    // printf("%d\n",(int*) mems_get((int*)0));
 
+    /*
+    In this section we are tring to write value to 1st index of array[0] (here it is 0 based indexing).
+    We get get value of both the 0th index and 1st index of array[0] by using function mems_get.
+    Then we write value to 1st index using 1st index pointer and try to access it via 0th index pointer.
 
+    This section is show that even if we have allocated an array using mems_malloc but we can 
+    retrive MeMS physical address of any of the element from that array using mems_get. 
+    */
     printf("\n------ Assigning value to Virtual address [mems_get] -----\n");
-
     // how to write to the virtual address of the MeMS (this is given to show that the system works on arrays as well)
     int* phy_ptr= (int*) mems_get(&ptr[0][1]); // get the address of index 1
     phy_ptr[0]=200; // put value at index 1
     int* phy_ptr2= (int*) mems_get(&ptr[0][0]); // get the address of index 0
     printf("Virtual address: %lu\tPhysical Address: %lu\n",(unsigned long)ptr[0],(unsigned long)phy_ptr2);
     printf("Value written: %d\n", phy_ptr2[1]); // print the address of index 1 
-    // all_subnodes();
+
+    /*
+    This shows the stats of the MeMS system.  
+    */
     printf("\n--------- Printing Stats [mems_print_stats] --------\n");
     mems_print_stats();
 
+    /*
+    This section shows the effect of freeing up space on free list and also the effect of 
+    reallocating the space that will be fullfilled by the free list.
+    */
     printf("\n--------- Freeing up the memory [mems_free] --------\n");
-    mems_free(ptr[0]);
+    mems_free(ptr[3]);
     mems_print_stats();
     ptr[3] = (int*)mems_malloc(sizeof(int)*250);
     mems_print_stats();
+
+    printf("\n--------- Unmapping all memory [mems_finish] --------\n\n");
     mems_finish();
     mems_print_stats();
     return 0;
